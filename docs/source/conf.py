eceeba0f50b2ab93bf34d88f7dacc1a1155ce1ab
Daniel Neel <dneelyep@gmail.com>
2024-09-05 19:09:25 -0400

    Fix page theming.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Adulting 101'
+copyright = '2021, Daniel Neel'
+author = 'Daniel Neel'
 
 release = '0.1'
 version = '0.1.0'
